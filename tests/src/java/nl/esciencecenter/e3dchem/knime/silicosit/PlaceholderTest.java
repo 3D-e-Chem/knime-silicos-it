479380d3324135e3824a444cf32e81242e58995a
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-07-21 14:37:25 +0200

    Build infrastructure expects a test so added a dummy test

---
+++ tests/src/java/nl/esciencecenter/e3dchem/knime/silicosit/PlaceholderTest.java
@@ -0,0 +1,14 @@
+package nl.esciencecenter.e3dchem.knime.silicosit;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class PlaceholderTest {
+
+	@Ignore("Nodes inside metanodes are copied each time, making workflow tests useless")
+	@Test
+	public void test() {
+		
+	}
+
+}

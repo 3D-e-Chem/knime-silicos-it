e2c2348dbba1d7afbb41722c6b7ae76715ee81d3
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-08-09 08:34:29 +0200

    Added tests for Activator and Execute node

---
+++ tests/src/java/nl/esciencecenter/e3dchem/knime/silicosit/ActivatorTest.java
@@ -0,0 +1,15 @@
+package nl.esciencecenter.e3dchem.knime.silicosit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ActivatorTest {
+
+	@Test
+	public void test() {
+		String expected = "nl.esciencecenter.e3dchem.knime.silicosit.plugin";
+		assertEquals(expected, Activator.PLUGIN_ID);
+	}
+
+}
---
+++ tests/src/java/nl/esciencecenter/e3dchem/knime/silicosit/execute/WorkflowTest.java
@@ -0,0 +1,37 @@
+package nl.esciencecenter.e3dchem.knime.silicosit.execute;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.workflow.UnsupportedWorkflowVersionException;
+import org.knime.core.util.LockFailedException;
+import org.knime.testing.core.TestrunConfiguration;
+
+import nl.esciencecenter.e3dchem.knime.testing.TestFlowRunner;
+
+public class WorkflowTest {
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
+	private TestFlowRunner runner;
+
+	@Before
+	public void setUp() {
+		TestrunConfiguration runConfiguration = new TestrunConfiguration();
+		runConfiguration.setLoadSaveLoad(false);
+		runConfiguration.setTestDialogs(true);
+		runner = new TestFlowRunner(collector, runConfiguration);
+	}
+
+	@Test
+	public void test_version() throws IOException, InvalidSettingsException, CanceledExecutionException,
+			UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
+		File workflowDir = new File("src/knime/execute-nod-tests");
+		runner.runTestWorkflow(workflowDir);
+	}
+}
